4dbc6d281e54c0670fbc62567b531175fd6205dc
kitsou <kitsunims@icloud.com>
2022-07-10 17:34:49 +0200

    created a window

--- src/me/kitsou/kas/KasApp.java
+++ src/me/kitsou/kas/KasApp.java
@@ -3,5 +3,8 @@ package me.kitsou.kas;
 public class KasApp {
     public static void main(String[] args){
         System.out.println("Demat d'ar bed !");
+
+        KasWindow window = new KasWindow("kas - STILL IN PROTOTYPE PHASE");
+        window.setVisible(true);
     }
 }
--- src/me/kitsou/kas/KasWindow.java
+++ src/me/kitsou/kas/KasWindow.java
@@ -1,4 +1,20 @@
 package me.kitsou.kas;
 
-public class KasWindow {
+import javax.swing.*;
+import java.awt.*;
+
+public class KasWindow extends JFrame {
+    public KasWindow(String title){
+        setTitle(title);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+        setSize(600, 400);
+        setLocationRelativeTo(null);
+
+        JPanel contentPane = (JPanel) this.getContentPane();
+        contentPane.setLayout(new FlowLayout());
+
+        JLabel label = new JLabel("Welcome to kas.");
+        contentPane.add(label);
+    }
 }
